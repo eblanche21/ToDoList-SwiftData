91c7d772613c2028939085170714be8b66a7abc3
Ethan Blanche <blancheq@bc.edu>
2024-10-15 17:09:56 -0400

    Now with updates!

--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ToDoListView.swift
 //  ToDoList
 //
 //  Created by Ethan Blanche on 10/15/24.
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    var toDos = ["Learn Swift", "Build Apps", "Change the World", "Bring the Awesome", "Take a Vacation"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(toDos, id: \.self) { toDo in
+                    NavigationLink {
+                        DetailView(passedValue: toDo)
+                    } label: {
+                        Text(toDo)
+                    }
+                }
+            }
+            .navigationTitle("To Do List")
+            .navigationBarTitleDisplayMode(.automatic)
+            .listStyle(.plain)
         }
-        .padding()
     }
 }
 
