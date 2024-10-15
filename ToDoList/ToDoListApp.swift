0237a4e341ef8cdacd27f8a06baa1502599ff461
Ethan Blanche <blancheq@bc.edu>
2024-10-15 15:53:07 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ethan Blanche on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
